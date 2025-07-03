43d95ce31c8007295ea01277db55daab19472914
Murilo <murkuehne@gmail.com>
2025-07-03 10:38:20 -0300

    Criando Componente de Input

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,10 @@
+import { Input } from "../components/Input"
+
 export function SignIn() {
   return (
-    <div>
-      <h1>SignIn</h1>
-    </div>
+    <form className="flex w-full flex-col gap-4">
+      <Input required legend="Email" type="email" placeholder="seu@email.com" />
+      <Input required legend="Senha" type="password" placeholder="senha" />
+    </form>
   )
 }
