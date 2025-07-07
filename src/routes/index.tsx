428b2572c63cdd97b59173e3ce143a400a20abfa
Murilo <murkuehne@gmail.com>
2025-07-07 09:38:08 -0300

    Creating the Dashboard Page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route } from "react-router"
 import { AuthRoutes } from "./Authroutes"
 import { EmployeeRoutes } from "./EmployeeRoutes"
+import { ManagerRoutes } from "./ManagerRoutes"
 
 export function Routes() {
   return (
     <BrowserRouter>
       {/* <AuthRoutes /> */}
-      <EmployeeRoutes />
+      <ManagerRoutes />
     </BrowserRouter>
   )
 }
